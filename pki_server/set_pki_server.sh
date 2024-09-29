3099c6897c3c2946a4e79d3fd4fbaad5d1d39d7f
Маховик Никита Сергеевич <n.mahovik@prosoftsystems.ru>
2024-09-29 09:25:07 +0500

    add artifacts and script fix

--- pki_server/set_pki_server.sh
+++ pki_server/set_pki_server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+PREFIX="/etc/easyrsa"
+
 # install all needed packages
 echo iptables-persistent iptables-persistent/autosave_v4 boolean false | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean false | sudo debconf-set-selections
 sudo apt update && yes | sudo apt install ./pki-server_0.1-1_all.deb ./security-settings_0.1-1_all.deb -y || exit 1
 
-PREFIX="/etc/easyrsa"
 /etc/iptables/security_settings.sh &&
 $PREFIX/init-pki.sh &&
 $PREFIX/build_ca.sh
